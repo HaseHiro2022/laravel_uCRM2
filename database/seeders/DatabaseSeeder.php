bde1ba51e509fcd8804a1b358c897aa9774cb2fc
HaseHiro2022 <hh_behappy-web@yahoo.co.jp>
2022-10-03 02:07:10 +0900

    sync

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,11 +21,10 @@ class DatabaseSeeder extends Seeder
         ]);
         
         \App\Models\Customer::factory(1000)->create();
-        \App\Models\Purchase::factory(100)->create();
 
         $items = \App\Models\Item::all();
 
-        Purchase::factory(100)->create()
+        Purchase::factory(10000)->create()
         ->each(function(Purchase $purchase) use ($items){
         $purchase->items()->attach(
         $items->random(rand(1,3))->pluck('id')->toArray(),
